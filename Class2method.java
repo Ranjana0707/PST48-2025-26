d3737255c87cf3482479e568757a1c11b6cff048
Ranjanasri P <ranjanaranjana7733@gmail.com>
2026-02-13 09:17:58 +0530

    Add files via upload

---
+++ Class2method.java
@@ -0,0 +1,30 @@
+package PST2;
+
+public class Class2method {
+
+	public static void main(String[] args) {
+	flower.lilly();
+	fruit.banana();
+	food.biriyani();
+
+	}
+}
+
+class flower {
+	public static void lilly () {
+		System.out.println("lilly is a flower");
+	}
+   }
+class fruit {
+	public static void banana() {
+		System.out.println("banana is a fruit");
+	}
+}
+
+class food{
+	public static void biriyani() {
+		System.out.println("biriyani is a food ");
+	}
+}
+    
+   
