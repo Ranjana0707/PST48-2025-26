6dcc138f7e6dceb1246a473bae822bae27f78630
Ranjanasri P <ranjanaranjana7733@gmail.com>
2026-02-13 09:24:36 +0530

    Add files via upload

---
+++ Intrestcalculation.java
@@ -0,0 +1,24 @@
+package PST;
+import java.util.Scanner;
+
+public class Intrestcalculation {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int P = 1000;
+		int R = 5;
+		int T = 2;
+		
+		
+		int SI =P*R*T/100;
+		
+		
+		System.out.println("Simple Intrest:" + SI);
+		
+		
+		
+		
+
+	}
+
+}
