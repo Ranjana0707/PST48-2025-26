cf7b74b3162ae67294c61b974553dd16c5e6179f
Ranjanasri P <ranjanaranjana7733@gmail.com>
2026-02-13 09:12:55 +0530

    Add files via upload

---
+++ MainMethod.java
@@ -0,0 +1,16 @@
+package PST2;
+import java.util.Scanner;
+
+public class MainMethod { 
+  public static void main(String[] args) {
+	  method ();
+  }
+    	
+    	 public static void method()
+   {
+	 for (int i=1;i<=5;i++)
+	   
+	   System.out.println("Ranjana");
+   }
+
+}
