2d80862f88196ab59ebcb5b23cf56455691238b8
Ranjanasri P <ranjanaranjana7733@gmail.com>
2026-02-13 09:36:36 +0530

    Add files via upload

---
+++ polimorphisam.java
@@ -0,0 +1,18 @@
+package PST2;
+
+public class polimorphisam {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
+
+class classA{
+	void CSE() {
+		return 0;
+	}
+}
+
+class 
