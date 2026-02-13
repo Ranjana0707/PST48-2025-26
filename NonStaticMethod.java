c537d6799f0f5a7a5d0acdb75262a37b01587433
Ranjanasri P <ranjanaranjana7733@gmail.com>
2026-02-13 09:14:38 +0530

    Add files via upload

---
+++ NonStaticMethod.java
@@ -0,0 +1,70 @@
+package PST2;
+
+public class NonStaticMethod {
+
+	public static void main(String[] args) {
+		
+	It1 obj= new It1();
+	obj.vino();
+	
+	It2 obj1= new It2();
+	obj1.sri();
+	
+	It3 obj2= new It3();
+	obj2.hari();
+	
+	It4 obj3= new It4();
+	obj3.sk();
+	
+	It5 obj4= new It5();
+	obj4.surya();
+	
+	It6 obj5=new It6();
+	obj5.ranjana();
+	
+	
+
+	}
+
+}
+
+
+ 
+     class It1 { 
+    	 public static void vino() {
+    	 System.out.println("vino is First Year");
+    	 
+    
+     }
+    	 
+     }
+     class It2 {
+    	 public static void sri() {
+    		 System.out.println("sri is Second Year");
+    	 }
+     }
+    	 
+    	
+     class It3 {
+    	 public static void hari() {
+    		 System.out.println("hari is third Year");
+    	 }
+    	 
+     }
+     class It4 {
+    	 public static void sk() {
+    		 System.out.println("sk is Final Year");
+    	 }
+    	 
+     }
+     class It5 {
+    	 public static void surya() {
+    		 System.out.println("surya is topper");
+    	 }
+     }
+     class It6 {
+    	 public static void ranjana() {
+    		 System.out.println("ranjana is a girl");
+    	 }
+     }
+     
