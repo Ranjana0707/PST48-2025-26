471e282dac6f8e29dbdb95a221c596cda151a3cb
Ranjanasri P <ranjanaranjana7733@gmail.com>
2026-02-13 09:36:16 +0530

    Add files via upload

---
+++ Palindrome.java
@@ -0,0 +1,26 @@
+package PST;
+import java.util.Scanner;
+
+   class Palindrome{
+	    public static void main(String[] args) {
+	        Scanner sc = new Scanner(System.in);
+
+	        System.out.print("Enter a string: ");
+	        String str = sc.nextLine();
+
+	        String rev = "";
+	        str = str.toLowerCase(); 
+
+	        for (int i = str.length() - 1; i >= 0; i--) {
+	            rev = rev + str.charAt(i);
+	        }
+
+	        if (str.equals(rev)) {
+	            System.out.println("The string is a Palindrome");
+	        } else {
+	            System.out.println("The string is NOT a Palindrome");
+	        }
+
+	        sc.close();
+	    }
+	}
