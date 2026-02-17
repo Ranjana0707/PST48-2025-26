42b548b774b5b8e92c5c8e0a55ce25ab15fb8ac9
Ranjanasri P <ranjanaranjana7733@gmail.com>
2026-02-17 19:47:06 +0530

    Add files via upload

---
+++ ChartoStr.java
@@ -0,0 +1,19 @@
+package JAVA;
+
+import JAVA.INTtoSTR.Converter;
+
+public class ChartoStr {
+
+			static class Converter {
+				public static void main(String[]args) {
+			
+					String name = "ranjana";
+					
+					char ch = name.charAt(0);
+					
+					System.out.println("String value:" + name);
+					System.out.println("Converted Charecter:"+ch);
+				}
+			}
+			
+}
---
+++ INTtoSTR.java
@@ -0,0 +1,19 @@
+package JAVA;
+
+public class INTtoSTR {
+	static class Converter {
+		static void convert(String Str) {
+		int num = Integer.parseInt(Str);
+			
+			System.out.println("Convert string value:" + num);
+		}
+		}
+	public static void main(String[]args){
+	{
+		String str = "987";
+		
+	    System.out.println("Integer value:" + str);
+	    Converter . convert(str);
+	}
+}
+}
